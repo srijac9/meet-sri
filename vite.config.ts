bb5c8df9ed3933e7e1ed1288215effbe8997c056
Srija Chitturi <srija.chitturi@missionperform.com>
2026-02-17 10:12:10 -0500

    Added Base to Vite.Config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/meet-sri/",
+
   assetsInclude: ["**/*.JPG"],
+
   server: {
     host: "::",
     port: 8080,
@@ -13,7 +16,9 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
