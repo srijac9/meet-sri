a68c5b4ec7529ee672a916311bc51982880df2ce
Srija Chitturi <schitturi0909@gmail.com>
2026-02-17 22:09:59 -0500

    sound graph working projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import FloatingHearts from "@/components/FloatingHearts";
 import MusicPlayerCard from "@/components/MusicPlayerCard";
+import SubtleAudioBackdrop from "@/components/SubtleAudioBackdrop";
 import p1 from "@/assets/p1.jpg";
 import p2 from "@/assets/p2.jpg";
 import p3 from "@/assets/p3.jpg";
@@ -10,6 +11,7 @@ import p6 from "@/assets/p6.jpg";
 import p9 from "@/assets/p9.jpg";
 import p10 from "@/assets/p10.jpg";
 import projectsHeader from "@/assets/projects-portfolio.png";
+import favouriteSong from "@/assets/favourite-song.mp3";
 
 const projectItems = [
   { title: "Portfolio Site", subtitle: "React - Tailwind", imageUrl: p1 },
@@ -35,16 +37,25 @@ const Projects = () => {
           {"<"} back home
         </Link>
 
-        <div className="mt-24" style={{ paddingLeft: "2rem" }}>
-          <img
-            src={projectsHeader}
-            alt="Projects Portfolio"
-            className="mb-8 block object-contain"
-            style={{ width: "30rem", maxWidth: "50rem", height: "auto" }}
-          />
+        <div className="mt-24 w-full px-8">
+          <div className="grid grid-cols-1 items-start gap-6 lg:grid-cols-[auto_1fr]">
+            <img
+              src={projectsHeader}
+              alt="Projects Portfolio"
+              className="block object-contain"
+              style={{ width: "30rem", maxWidth: "100%", height: "auto" }}
+            />
+            <SubtleAudioBackdrop
+              src={favouriteSong}
+              title="Now Playing"
+              subtitle="Add src/file to react with your track"
+              autoStart={false}
+              height={290}
+            />
+          </div>
         </div>
 
-        <div className="mt-4 w-full px-4">
+        <div className="mt-6 w-full px-4">
           <div className="grid w-full grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
             {projectItems.map((project) => (
               <MusicPlayerCard
